0e3eb29d91df67f41cf9f210f9cfaef2ab95180b
Dom Del Nano <ddelnano@gmail.com>
2019-04-11 22:18:04 -0700

    Refactor getAllObject rpc calls into generic method

--- client/template.go
+++ client/template.go
@@ -1,12 +1,5 @@
 package client
 
-import (
-	"context"
-	"errors"
-	"fmt"
-	"time"
-)
-
 type Template struct {
 	// TODO: Not sure the difference between these two
 	Id        string
@@ -14,53 +7,32 @@ type Template struct {
 	NameLabel string
 }
 
+func (t Template) Compare(obj map[string]interface{}) bool {
+	name_label := obj["name_label"].(string)
+	if t.NameLabel == name_label {
+		return true
+	}
+	return false
+}
+
+func (p Template) New(obj map[string]interface{}) XoObject {
+	id := obj["id"].(string)
+	uuid := obj["uuid"].(string)
+	name_label := obj["name_label"].(string)
+	return Template{
+		Id:        id,
+		NameLabel: name_label,
+		Uuid:      uuid,
+	}
+}
+
 func (c *Client) GetTemplate(name string) (Template, error) {
-	var template Template
-	params := map[string]interface{}{
-		"type": "VM-template",
-	}
-	var objsRes struct {
-		Templates map[string]interface{} `json:"-"`
-	}
-	ctx, _ := context.WithTimeout(context.Background(), 100*time.Second)
-	err := c.rpc.Call(ctx, "xo.getAllObjects", params, &objsRes.Templates)
+	obj, err := c.FindFromGetAllObjects(Template{NameLabel: name})
+	template := obj.(Template)
 
 	if err != nil {
 		return template, err
 	}
 
-	found := false
-	for _, obj := range objsRes.Templates {
-		v, ok := obj.(map[string]interface{})
-		if !ok {
-			return template, errors.New("Could not coerce interface{} into map")
-		}
-
-		if v["type"].(string) != "VM-template" {
-			continue
-		}
-
-		name_label, ok := v["name_label"].(string)
-
-		if !ok {
-			return template, errors.New(fmt.Sprintf("type assertion for name_label failed on VM-template: %v", v))
-		}
-
-		if name_label == name {
-			found = true
-			// TODO: Add stricter error checking here
-			id := v["id"].(string)
-			uuid := v["uuid"].(string)
-			template = Template{
-				Id:        id,
-				NameLabel: name_label,
-				Uuid:      uuid,
-			}
-		}
-	}
-
-	if !found {
-		return template, NotFound{Type: "VM-template"}
-	}
 	return template, nil
 }
