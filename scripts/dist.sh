68a1c11a71c0786e5f95fdf181c61a3be40cbb04
Dom Del Nano <ddelnano@gmail.com>
2019-09-27 18:58:38 -0700

    Fix script file

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -3,7 +3,7 @@
 export GOARCH=amd64
 for OS in linux darwin; do
     export GOOS=$OS
-    file=dist/terraform-provider-mikrotik-${TRAVIS_TAG}-${GOOS}_${GOARCH}
+    file=dist/terraform-provider-xenorchestra-${TRAVIS_TAG}-${GOOS}_${GOARCH}
     go build -o ${file}
     sha256sum ${file} > "${file}.sha256sum"
 done
