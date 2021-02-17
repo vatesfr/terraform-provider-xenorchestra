793aac4214472038e873166bfacf747656811374
Deniz Cakan <deniz.cakan@internetx.com>
2021-02-17 20:36:15 +0100

    draft for tests

---
+++ xoa/data_source_hosts_test.go
@@ -0,0 +1,51 @@
+package xoa
+
+import (
+	"fmt"
+	"log"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+)
+
+func TestAccXenorchestraDataSource_hosts(t *testing.T) {
+	resourceName := "data.xenorchestra_hosts.hosts"
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccXenorchestraDataSourceHostsConfig(),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccCheckXenorchestraDataSourceHosts(resourceName),
+					resource.TestCheckResourceAttrSet(resourceName, "id"),
+					resource.TestCheckResourceAttr(resourceName, "pool", accTestHost.Pool)),
+			},
+		},
+	},
+	)
+}
+
+func testAccCheckXenorchestraDataSourceHosts(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Can't find Hosts data source: %s", n)
+		}
+
+		log.Printf("[DEBUG] Found resource again %v", s.RootModule().Resources)
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("Hosts data source ID not set")
+		}
+		return nil
+	}
+}
+
+func testAccXenorchestraDataSourceHostsConfig() string {
+	return fmt.Sprintf(`
+data "xenorchestra_hosts" "hosts" {
+    pool = "%s"
+}
+`, accTestPool.Id)
+}
