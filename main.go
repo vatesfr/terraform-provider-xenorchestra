8e25fa3491b4c4e8a3dd620204317c0c57077c4c
Dom Del Nano <ddelnano@gmail.com>
2021-06-27 20:26:59 -0700

    Add debug mode to provider

--- main.go
+++ main.go
@@ -1,13 +1,30 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"log"
+
 	"github.com/ddelnano/terraform-provider-xenorchestra/xoa"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
 func main() {
 
-	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: xoa.Provider,
-	})
+	var debugMode bool
+	flag.BoolVar(&debugMode, "debuggable", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	if debugMode {
+		err := plugin.Debug(context.Background(), "registry.terraform.io/terra-farm/xenorchestra",
+			&plugin.ServeOpts{
+				ProviderFunc: xoa.Provider,
+			})
+		if err != nil {
+			log.Println(err.Error())
+		}
+	} else {
+		plugin.Serve(&plugin.ServeOpts{
+			ProviderFunc: xoa.Provider})
+	}
 }
