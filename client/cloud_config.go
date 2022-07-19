54a9e85feeb4f17fd7e6561e0ae6285c915f2956
Dom Del Nano <ddelnano@gmail.com>
2022-07-18 23:14:01 -0700

    Fix cloud config issue related to breaking change in XO api (#206)
    * Ensure cloudConfig.create is marshalled into interface{} to fix bug in backwards compatible way
    
    * Fix comment

--- client/cloud_config.go
+++ client/cloud_config.go
@@ -86,7 +86,11 @@ func (c *Client) CreateCloudConfig(name, template string) (*CloudConfig, error) 
 		"name":     name,
 		"template": template,
 	}
-	var resp bool
+	// Xen Orchestra versions >= 5.98.0 changed this return value to an object
+	// when older versions returned bool. This needs to be an interface
+	// type in order to be backwards compatible while fixing this bug. See
+	// GitHub issue 204 for more details.
+	var resp interface{}
 	err := c.Call("cloudConfig.create", params, &resp)
 
 	if err != nil {
