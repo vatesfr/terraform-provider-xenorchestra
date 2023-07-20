ef8d6c607843440bd666d49fe75ef9ac20e3eadb
Dom Del Nano <ddelnano@gmail.com>
2023-07-19 22:36:43 -0700

    Remove uncommented code

--- client/setup_test.go
+++ client/setup_test.go
@@ -52,7 +52,7 @@ func TestMain(m *testing.M) {
 	FindHostForTests(accTestPool.Master, &accTestHost)
 	FindStorageRepositoryForTests(accTestPool, &accDefaultSr, integrationTestPrefix)
 	CreateNetwork(&accDefaultNetwork)
-	// FindOrCreateVmForTests(&accVm, accTestPool.Id, accDefaultSr.Id, testTemplate.Id, integrationTestPrefix)
+	FindOrCreateVmForTests(&accVm, accTestPool.Id, accDefaultSr.Id, testTemplate.Id, integrationTestPrefix)
 	CreateResourceSet(testResourceSet)
 
 	code := m.Run()
