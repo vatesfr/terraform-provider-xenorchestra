caccc64e22f4d194d142d8ce55b0fc7ccef27f81
Dom Del Nano <ddelnano@gmail.com>
2023-12-29 10:48:28 -0800

    Use same go version as provider, use build tags and tools target to pull in tfplugindocs

--- main.go
+++ main.go
@@ -9,6 +9,10 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
+// Generate the Terraform provider documentation using `tfplugindocs`:
+//
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
+
 func main() {
 
 	var debugMode bool
